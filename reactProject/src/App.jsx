801411135bb0e0357b8dc745dbb0e362728fddf7
ciencix16 <oliwiercienciala07@gmail.com>
2025-04-07 21:05:28 +0200

    .

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -2,6 +2,8 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
   return (
@@ -9,6 +11,11 @@ function App() {
       <Header />
       <Food />
       <Card />
+      <Button />
+      <Student name="Krystian" age={20} isStudent={true} />
+      <Student name="Sasha" age={34} isStudent={false} />
+      <Student name="Pyndzel" age={12} isStudent={false} />
+      <Student />
       <Footer />
     </>
   );
